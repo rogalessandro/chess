ba044bfecfcf4f15a5cead6e993e8f200c93ad34
Alessandro <rogedigo.racc@gmail.com>
2025-03-03 19:39:52 -0700

    Finish making sure the test that I did are correct. Still working on JoinGame

---
+++ server/src/test/java/dataaccess/ListGamesServiceTest.java
@@ -0,0 +1,53 @@
+package dataaccess;
+
+
+import dataaccess.*;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.*;
+import service.ListGamesService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListGamesServiceTest {
+    private MemoryGameDAO gameDAO;
+    private MemoryAuthDAO authDAO;
+    private ListGamesService listGamesService;
+    private String authToken;
+
+    @BeforeEach
+    public void CreaUtilidades() {
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        listGamesService = new ListGamesService(gameDAO, authDAO);
+
+
+        authToken = "Token123";
+        authDAO.insertAuth(new AuthData(authToken, "UsuarioPrueba"));
+    }
+
+    @Test
+    public void testMalToken() {
+
+        assertThrows(DataAccessException.class, () -> listGamesService.listGames("TokenMal"));
+    }
+
+    @Test
+    public void testListaHecha() throws DataAccessException {
+
+        gameDAO.insertGame(new GameData(1, null, null, "Game 1", new chess.ChessGame()));
+        gameDAO.insertGame(new GameData(2, null, null, "Game 2", new chess.ChessGame()));
+
+
+        List<GameData> games = listGamesService.listGames(authToken);
+
+
+        assertEquals(2, games.size());
+        assertEquals("Game 1", games.get(0).gameName());
+        assertEquals("Game 2", games.get(1).gameName());
+    }
+
+
+}
