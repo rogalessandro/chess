b4c68dfb125a9e77e7970e368e9d6113ef7f4099
Alessandro <rogedigo.racc@gmail.com>
2025-03-03 19:32:43 -0700

    Finish making sure the test that I did are correct

---
+++ server/src/test/java/dataaccess/CreateGameServiceTest.java
@@ -0,0 +1,44 @@
+package dataaccess;
+import dataaccess.*;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.*;
+import service.CreateGameService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CreateGameServiceTest {
+    private MemoryGameDAO gameDAO;
+    private MemoryAuthDAO authDAO;
+    private CreateGameService createGameService;
+    private String authToken;
+
+    @BeforeEach
+    public void CreaUtilidades() {
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        createGameService = new CreateGameService(gameDAO, authDAO);
+
+        authToken = "Token123";
+        authDAO.insertAuth(new AuthData(authToken, "UsuarioPrueba"));
+    }
+
+    @Test
+    public void testCrearUnJuegoExitoso() throws DataAccessException {
+
+        GameData game = createGameService.createGame(authToken, "JuegoPrueba");
+
+
+        assertNotNull(game);
+        assertEquals("JuegoPrueba", game.gameName());
+        assertNotNull(gameDAO.getGame(game.gameID()));
+    }
+
+    @Test
+    public void testJuegoPeroInvalidAuth() {
+
+        assertThrows(DataAccessException.class, () -> createGameService.createGame("token malo", "Juego Inco"));
+    }
+
+
+}
