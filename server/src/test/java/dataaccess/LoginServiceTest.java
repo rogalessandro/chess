6a4de766a8555fb071ca09ba31344cd7dc0ed959
Alessandro <rogedigo.racc@gmail.com>
2025-03-03 19:13:54 -0700

    Finish making sure the tests that I did are correct

---
+++ server/src/test/java/dataaccess/LoginServiceTest.java
@@ -0,0 +1,48 @@
+package dataaccess;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import service.AuthService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginServiceTest {
+    private MemoryUserDAO userDAO;
+    private MemoryAuthDAO authDAO;
+    private AuthService authService;
+
+    @BeforeEach
+    public void CreacionDeUtilidadess() throws DataAccessException {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        authService = new AuthService(userDAO, authDAO);
+
+
+        userDAO.insertUser(new UserData("UsuarioDePrueba", "contra123", "test@email.com"));
+    }
+
+
+    @Test
+    public void testLoginContrasenaMala() {
+
+        assertThrows(DataAccessException.class, () -> authService.login("UsuarioDePrueba", "contraIncorrecta"));
+    }
+
+    @Test
+    public void testLoginExitoso() throws DataAccessException {
+
+        String authToken = authService.login("UsuarioDePrueba", "contra123");
+
+
+        assertNotNull(authToken);
+        assertNotNull(authDAO.getAuth(authToken));
+    }
+
+    @Test
+    public void testLoginUsuarioNoExiste() {
+
+        assertThrows(DataAccessException.class, () -> authService.login("fakeUser", "password"));
+    }
+}
---
+++ server/src/test/java/dataaccess/LogoutServiceTest.java
@@ -0,0 +1,40 @@
+package dataaccess;
+
+import dataaccess.*;
+import model.AuthData;
+import org.junit.jupiter.api.*;
+import service.LogoutService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LogoutServiceTest {
+    private MemoryAuthDAO authDAO;
+    private LogoutService logoutService;
+    private String authToken;
+
+    @BeforeEach
+    public void CreaUtilidades() {
+        authDAO = new MemoryAuthDAO();
+        logoutService = new LogoutService(authDAO);
+
+
+        authToken = "Token123";
+        authDAO.insertAuth(new AuthData(authToken, "UsuarioDePrueba"));
+    }
+
+
+    @Test
+    public void LogoutTokenIncorrecto() {
+
+        assertThrows(DataAccessException.class, () -> logoutService.logout("invalidToken456"));
+    }
+
+    @Test
+    public void LogoutExitoso() throws DataAccessException {
+
+        logoutService.logout(authToken);
+
+
+        assertNull(authDAO.getAuth(authToken));
+    }
+}
