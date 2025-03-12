9053887918ce62e5ca6d756e56c3926ae91ba5a3
Alessandro <rogedigo.racc@gmail.com>
2025-03-12 16:05:22 -0600

    I finished MySQLAuthDAO very fast as I just copied and modify my code from the MySQLUserDAO class

--- server/src/main/dataaccess/MySQLAuthDAO.java
+++ server/src/main/dataaccess/MySQLAuthDAO.java
@@ -42,19 +42,93 @@ public class MySQLAuthDAO implements AuthDAO {
 
 
     public AuthData getAuth(String token) throws DataAccessException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet resultSet = null;
+        try {
+            conn = DatabaseManager.getConnection();
 
-        return null;
+
+            String sql = "SELECT users.username FROM auth_tokens JOIN users ON auth_tokens.user_id = users.id WHERE token = ?";
+            stmt = conn.prepareStatement(sql);
+
+
+            stmt.setString(1, token);
+
+
+            resultSet = stmt.executeQuery();
+
+
+            if (resultSet.next()) {
+                return new AuthData(token, resultSet.getString("username"));
+            } else {
+                //No hay token
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("No se cerro las conexiones, por que?");
+            }
+        }
     }
 
 
     public void deleteAuth(String token) throws DataAccessException {
-
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = DatabaseManager.getConnection();
+            String sql = "DELETE FROM auth_tokens WHERE token = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, token);
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+                System.out.println("Intento borrar, No Token encontrado");
+            } else {
+                System.out.println("Borrado con exito");
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("No se cerro las conexiones, por que?");
+            }
+        }
     }
 
 
     public void clear() throws DataAccessException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try{
+            conn = DatabaseManager.getConnection();
 
+            String sql = "DELETE FROM auth_tokens";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.executeUpdate();
+
+        }catch(SQLException e){
+            throw new DataAccessException(e.getMessage());
+        }finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("No se cerro las conexiones, por que?");
+            }
+        }
     }
+
 
 
 }
