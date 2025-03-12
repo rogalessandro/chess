62540739f14746805f1a0f3667872825f289c609
Alessandro <rogedigo.racc@gmail.com>
2025-03-12 15:23:00 -0600

    Done with insert and get user in MySQLUserDAO

--- server/src/main/dataaccess/MySQLUserDAO.java
+++ server/src/main/dataaccess/MySQLUserDAO.java
@@ -34,17 +34,50 @@ public class MySQLUserDAO implements UserDAO {
         } catch (SQLException e) {
             System.out.println("No se inserto el usuario, paso algo");
             throw new DataAccessException(e.getMessage());
+        }finally {
+
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("No se cerro las conexiones, por que?");
+            }
+
         }
     }
 
 
-    @Override
     public UserData getUser(String username) throws DataAccessException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet resultSet = null;
 
-        return null;
+        try{
+            conn = DatabaseManager.getConnection();
+
+            String sqlBuscarUsuario = "SELECT * FROM users WHERE username = ?";
+            stmt = conn.prepareStatement(sqlBuscarUsuario);
+
+            stmt.setString(1, username);
+            stmt.executeQuery();
+
+            if(resultSet.next()){
+                return new UserData(resultSet.getString("username"),
+                        resultSet.getString("password"), resultSet.getString("email"));
+
+            }else{
+                //No hay usuario
+                return null;
+            }
+
+        } catch (SQLException e){
+            throw new DataAccessException(e.getMessage());
+
+        }
+
     }
 
-    @Override
+
     public void clear() throws DataAccessException {
 
     }
