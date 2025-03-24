a1c3d352625bd33f31d33bec3c3c4c1883dc8f12
Alessandro <rogedigo.racc@gmail.com>
2025-03-24 13:19:50 -0600

    Also created some tests for login and register. Created a clearDatabase method. Just positives for now

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,29 @@
 package client;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
+    private static int port;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
+        facade = new ServerFacade(port);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -19,6 +31,46 @@ public class ServerFacadeTests {
     static void stopServer() {
         server.stop();
     }
+
+
+    @BeforeEach
+    void clearServer() throws Exception {
+        URL url = new URL("http://localhost:" + port + "/db");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("DELETE");
+
+        int status = connection.getResponseCode();
+        if (status != 200) {
+            throw new RuntimeException("Failed to clear database. Status: " + status);
+        }
+    }
+
+
+
+    @Test
+    public void registerPositive() throws Exception {
+        AuthData auth = facade.register("testuser123", "password", "test123@email.com");
+
+        assertNotNull(auth);
+        assertEquals("testuser123", auth.username());
+        assertTrue(auth.authToken().length() > 10);
+    }
+
+
+
+    @Test
+    void loginPositive() throws Exception {
+        facade.register("juanito", "1234", "juan@email.com");
+        AuthData auth = facade.login("juanito", "1234");
+
+        assertNotNull(auth);
+        assertEquals("juanito", auth.username());
+        assertTrue(auth.authToken().length() > 5);
+    }
+
+
+
+
 
 
     @Test
