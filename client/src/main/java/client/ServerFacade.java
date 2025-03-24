8f878fd8e7fe16feb463d8aa23f4ce466c600f62
Alessandro <rogedigo.racc@gmail.com>
2025-03-24 13:09:31 -0600

    Done register method in Facade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,8 +1,9 @@
 package client;
+import chess.ChessGame;
 import model.AuthData;
+import model.GameData;
 import com.google.gson.Gson;
-
-
+import java.util.List;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,5 +19,40 @@ public class ServerFacade {
     }
 
     public AuthData register(String username, String password, String email) throws Exception {
+        var requestBody = Map.of(
+                "username", username,
+                "password", password,
+                "email", email
+        );
+
+        URL url = new URL(serverUrl + "/user");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream();
+             OutputStreamWriter writer = new OutputStreamWriter(os)) {
+            gson.toJson(requestBody, writer);
+            writer.flush();
+        }
+
+
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            InputStream errorStream = connection.getErrorStream();
+            String errorMessage = new BufferedReader(new InputStreamReader(errorStream))
+                    .lines()
+                    .reduce("", (acc, line) -> acc + line);
+            throw new RuntimeException("Error: " + errorMessage);
+        }
+
+        try (InputStream responseBody = connection.getInputStream()) {
+            var map = gson.fromJson(new InputStreamReader(responseBody), Map.class);
+            return new AuthData((String) map.get("authToken"), (String) map.get("username"));
+        }
+    }
+
+
+
 
 }
