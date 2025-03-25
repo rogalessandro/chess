3739d915d3e6480e652d5a9248588b16efb63cfc
Alessandro <rogedigo.racc@gmail.com>
2025-03-25 14:24:50 -0600

    autograder

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import chess.*;
 import client.ServerFacade;
-import server.Server;
 import ui.ChessClient;
 
 public class Main {
@@ -8,10 +7,7 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
-        Server server = new Server();
-        int port = server.run(8080);
-
-        ServerFacade facade = new ServerFacade(port);
+        ServerFacade facade = new ServerFacade(8080);
 
         ChessClient client = new ChessClient(facade);
         client.run();
