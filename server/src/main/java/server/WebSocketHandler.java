10d6a1400a4da597112773af33b8f13e0026c16a
Alessandro <rogedigo.racc@gmail.com>
2025-04-07 13:00:57 -0600

    Cretation of the WebSocket Handler and its methods to use

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,7 +1,26 @@
-package server;
-
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 @ServerEndpoint("/ws")
 public class WebSocketHandler {
+    private static final Map<Integer, Map<Session, String>> gameSessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        throwable.printStackTrace();
+    }
 }
